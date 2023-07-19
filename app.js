1ee2fa972dbdec454a3c14fb2495001a93f7a163
Tushar Kalra <tkalra1111@gmail.com>
2023-07-19 16:25:09 +0530

    Updated

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ const _ = require('lodash')
 
 const items = [1,[2,[3,[4,[5]]]]]
 const newItems = _.flattenDeep(items)
-console.log(newItems);
+console.log(newItems); 
+
+console.log('Hello People')
